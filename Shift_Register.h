1ebe9ffa875e7c00916b3dadb2a6d8ebf83e02b5
MrezaPbs <125213272+Mrezapbs@users.noreply.github.com>
2024-06-10 00:33:11 +0330

    Update Shift_Register.h

--- Shift_Register.h
+++ Shift_Register.h
@@ -1,2 +1,8 @@
+#ifndef _SHIFT_REGISTER_H_
+#define _SHIFT_REGISTER_H_
 #include "main.h"
-extern Update_Shift_Register
+void Shift_Register_Config(void);
+void Reset_Shift_Register(void);      
+void Update_Shift_Register(void);
+
+#endif
