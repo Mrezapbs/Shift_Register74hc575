41af46d97581c493d1a892e5bdb3b9ebedf66c6f
MrezaPbs <125213272+Mrezapbs@users.noreply.github.com>
2024-06-10 00:35:51 +0330

    Update Shift_Register.h

--- Shift_Register.h
+++ Shift_Register.h
@@ -1,6 +1,14 @@
 #ifndef _SHIFT_REGISTER_H_
 #define _SHIFT_REGISTER_H_
-#include "main.h"
+
+#define   SHIFT_REGISTER_SER_PORT    GPIOC
+#define   SHIFT_REGISTER_SCLK_PORT   GPIOC
+#define   SHIFT_REGISTER_RCLK_PORT   GPIOC
+
+#define   SHIFT_REGISTER_SER_PIN    GPIO_PIN_11
+#define   SHIFT_REGISTER_SCLK_PIN   GPIO_PIN_12
+#define   SHIFT_REGISTER_RCLK_PIN   GPIO_PIN_13
+
 void Shift_Register_Config(void);
 void Reset_Shift_Register(void);      
 void Update_Shift_Register(void);
